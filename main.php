73d6eda4c3d068d8c52652571ba65b992cda5cfa
gabriela <gabriela.s.souza@edu.senai.br>
2026-04-23 11:49:41 -0300

    v.0.3 - adicionado função de subtração ao projeto

--- main.php
+++ main.php
@@ -1,11 +1,16 @@
 <?php
 
 //funções em PHP
-echo "na aula do dia 23/04/2026, eu aprendi a mexer no github/git e desenvolver funções no php =D";
+echo "na aula do dia 23/04/2026, eu aprendi a mexer no github/git e desenvolver funções no php =D \n";
 
 function somar ($a, $b, $c){
     return $a + $b;
 } 
-echo "resultado: ", somar (5, 7, 3);
+
+function subtrair ($a, $b){
+    return $a - $b;
+}
+
+echo "resultado: ", subtrair (10, 5);
 
 ?>
